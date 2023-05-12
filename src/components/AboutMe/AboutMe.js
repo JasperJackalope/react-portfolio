69f460739dc8a77b37cde3f9b083c8266526b8c7
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-11 19:46:39 -0700

    Typos and Links

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <img src={headshotDB} alt="Daniel Boddicker" />
         <div>
           <p>Originally from the Black Hills of South Dakota, now based in Los Angeles for the past decade. After a successful career in the film and television industry, I decided to take the risk and change paths by becoming a web developer and coder. I am a recent gradutate of the UCLA Extension Coding Bootcamp and am looking for my next opportunity.</p>   
-          <p>Thank you for visiting my website. Here you can more information about my skills as a Full-Stack Developer along with samples of my work.</p>
+          <p>Thank you for visiting my website. Here you can see more information about my skills as a Full-Stack Developer along with samples of my work.</p>
         </div>
       </div>
     </section>
